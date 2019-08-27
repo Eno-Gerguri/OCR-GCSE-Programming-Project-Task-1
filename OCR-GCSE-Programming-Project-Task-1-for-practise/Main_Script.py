16ea1e4a509f8b63e6b4940f7ab3d88825f9c732
Eno-Gerguri <enogerguri@outlook.com>
2019-08-27 22:18:49 +0100

    Finished Files first version

--- OCR-GCSE-Programming-Project-Task-1-for-practise/Main_Script.py
+++ OCR-GCSE-Programming-Project-Task-1-for-practise/Main_Script.py
@@ -1,15 +1,41 @@
 from Authenticate_User import get_usernames_and_passwords, authenticate_user
-from Music_Game import *
+from Music_Game import get_artists_and_songs, start_music_game
+
+
+# ======================================================================================================================
+# ======================================================================================================================
+
+def main_menu():
+    print("Main Menu\n\n")
+
+    print("Play\n\nSettings\n\nSign Out\n\n")
+
+    picked_option = input()
+
+    print("\n\n")
+
+    if picked_option.lower().strip().replace(" ", "") == "play":
+        start_music_game()
+
+    elif picked_option.lower().strip().replace(" ", "") == "signout":
+        authenticate_user()  # Goes back to the sign in menu
+
+    else:
+        print("That is invalid. Check your spelling.\n\n")
+
+        main_menu()
 
 # ====================================================initialisation====================================================
+# ======================================================================================================================
 
 get_usernames_and_passwords()
 
 get_artists_and_songs()
 
 
+# ====================================================Program Starts====================================================
 # ======================================================================================================================
 
 authenticate_user()
 
-start_music_game()
+main_menu()
